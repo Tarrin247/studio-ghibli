ae437bb38eccc21c91780baa726ff55a4fc0c8cf
Timple Basinger <tbasinger@protonmail.com>
2021-10-21 13:29:41 -0500

    commit message

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,42 @@ import { useEffect, useState } from 'react';
 
 const App = () => {
     const [films, setFilms] = useState([]);
+    const [stars, setStars] = useState([]);
 
+    // useEffect(() => {
+    //     fetch('https://ghibliapi.herokuapp.com/films')
+    //         .then(res => res.json())
+    //         .then(allFilms => setFilms(allFilms))
+    // }, []);
 
-    useEffect(() => {
+    const getFilms = () => {
         fetch('https://ghibliapi.herokuapp.com/films')
             .then(res => res.json())
             .then(allFilms => setFilms(allFilms))
-    }, []);
+    };
 
+    const getStars = () => {
+        fetch('https://ghibliapi.herokuapp.com/people')
+            .then(res => res.json())
+            .then(allStars => setStars(allStars))
+    };
 
     return (
         <main className="container">
             <div class="btn-group" role="group" aria-label="Basic outlined example">
-                <button type="button" class="btn btn-outline-primary">Films</button>
-                <button type="button" class="btn btn-outline-primary">Stars</button>
+                <button onClick={() => getFilms()}
+                    type="button"
+                    class="btn btn-outline-dark">Films</button>
+                <button onClick={() => getStars()}
+                    type="button"
+                    class="btn btn-outline-dark">
+                    Stars
+                </button>
+                <button onClick="refreshPage()"
+                    type="button"
+                    class="btn btn-outline-dark">
+                    Home
+                </button>
             </div>
             <section className="row justify-content-center mt-5">
                 {films.map(film => (
@@ -33,7 +55,18 @@ const App = () => {
                             </div>
                         </div>
                     </div>
-
+                ))}
+            
+                {stars.map(star => (
+                    <div class="card" style={{width:"18 em"}} key={`star-card-${star.id}`}>
+                        <div className="card ">
+                            <h5 class="card-title">Name: {star.name}</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">Age: {star.age}</h6>
+                            <p class="card-text">Gender: {star.gender}</p>
+                            <a href="#" class="card-link">Films: {star.films}</a>
+                            <a href="#" class="card-link"></a>
+                        </div>
+                    </div>
                 ))}
             </section>
         </main>
